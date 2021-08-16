ce51703bf1939fdb5c0ab62f0380db83de251061
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-08-16 18:28:10 -0500

    improving thread

--- src/main/java/edu/eci/arsw/threads/HostBlackListThread.java
+++ src/main/java/edu/eci/arsw/threads/HostBlackListThread.java
@@ -1,19 +1,47 @@
 package edu.eci.arsw.threads;
 
+import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+
 /**
- * @author Ana Gabriela Silva
- * @author Juan Andrés Pico
  * @author Leonardo Galeano
  * @author Iván Camilo Rincón Saavedra
- * @version 8/16/2021
+ * @version version 1.0 8/16/2021
  */
 public class HostBlackListThread extends Thread{
+    private int a;
+    private int b;
+    private int ocurrencesCount=0;
+    private int checkedListsCount=0;
+    private static int blackListAlarmCount;
+    private HostBlacklistsDataSourceFacade skds;
 
-    public HostBlackListThread (String ipaddress){
+    String ipAddress;
 
+    public HostBlackListThread (int a, int b, String ipAddress, HostBlacklistsDataSourceFacade skds, int BLACK_LIST_ALARM_COUNT){
+        this.a = a;
+        this.b = b;
+        this.ipAddress = ipAddress;
+        this.skds = skds;
+        blackListAlarmCount = BLACK_LIST_ALARM_COUNT;
     }
+
 
     @Override
     public void run() {
+        for (int i=a;i<b && ocurrencesCount<blackListAlarmCount;i++){
+            checkedListsCount++;
+
+            if (skds.isInBlackListServer(i, ipAddress)){
+                ocurrencesCount++;
+            }
+        }
+    }
+
+    public int getOcurrencesCount() {
+        return ocurrencesCount;
+    }
+
+    public int getCheckedListsCount() {
+        return checkedListsCount;
     }
 }
